7599b92769c6b6611a55bf2b451ef3b2f0b660d1
Uilian Ries <uilian.ries@datacom.ind.br>
2016-03-02 08:58:25 -0300

    Fix Boost Unit Test Framework
    - Fixed link to boost libraries
    - Added validation for gpio_config
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test/test_config.cpp
+++ test/test_config.cpp
@@ -1,53 +1,61 @@
+#define BOOST_TEST_NO_LIB
+
 #include <string>
 
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
-#include <boost/test/unit_test_suite.hpp>
-#include <boost/test/included/unit_test.hpp>
+
+#include <boost/test/unit_test.hpp>
 
 #include "config.hpp"
 
 BOOST_AUTO_TEST_SUITE(ConfigGPIO)
 
-static const boost::filesystem::path fake_class_dir("fake_gpio_dir");
+static const boost::filesystem::path fake_class_dir("./fake_gpio_dir");
 static constexpr auto gpio_pin = 66;
+static const std::string pin = "gpio" + std::to_string(gpio_pin);
+static const boost::filesystem::path fake_gpio_dir = fake_class_dir / pin;
+static const boost::filesystem::path export_path = fake_class_dir / "export";
+static const boost::filesystem::path unexport_path = fake_class_dir / "unexport";
+static const boost::filesystem::path direction_path = fake_gpio_dir / "direction";
+static const boost::filesystem::path value_path = fake_gpio_dir / "value";
 
 struct fake_environment {
-    fake_environment() {
-       using boost::filesystem::path;
-       using boost::filesystem::create_directories;
+    template<typename T>
+    void create_file(T&& path) {
        using boost::filesystem::ofstream;
 
-       const std::string pin = "gpio" + std::to_string(gpio_pin);
-       const path fake_gpio_dir = fake_class_dir / pin;
-
-       create_directories(fake_gpio_dir);
-
-       const path export_path = fake_gpio_dir / "export";
-       ofstream ofs(export_path);
-       BOOST_CHECK(ofs);
-       ofs.close();
-
-       const path unexport_path = fake_gpio_dir / "unexport";
-       ofs.open(export_path);
-       BOOST_CHECK(ofs);
-       ofs.close();
-
-       const path direction_path = fake_gpio_dir / pin / "direction";
-       ofs.open(direction_path);
-       BOOST_CHECK(ofs);
-       ofs.close();
-
-       const path value_path = fake_gpio_dir / pin / "value";
-       ofs.open(value_path);
+       ofstream ofs(std::forward<T>(path));
        BOOST_CHECK(ofs);
        ofs.close();
     }
+
+    fake_environment() {
+       using boost::filesystem::path;
+       using boost::filesystem::create_directories;
+
+       create_directories(fake_gpio_dir);
+
+       create_file(export_path);
+       create_file(unexport_path);
+       create_file(value_path);
+       create_file(direction_path);
+    }
+
+    ~fake_environment() {
+      BOOST_CHECK(boost::filesystem::remove_all(fake_gpio_dir));
+    }
+
 };
 
 BOOST_FIXTURE_TEST_CASE(FakeEnvironment, fake_environment)
 {
    auto gconfig = bbb::gpio::config::make_config(fake_class_dir, gpio_pin);
+
+   BOOST_CHECK_EQUAL(export_path, gconfig->get_export());
+   BOOST_CHECK_EQUAL(unexport_path, gconfig->get_unexport());
+   BOOST_CHECK_EQUAL(value_path, gconfig->get_value());
+   BOOST_CHECK_EQUAL(direction_path, gconfig->get_direction());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
