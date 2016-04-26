57a44b9c4185b666ad8b39b3472dd8792d64dc5d
Uilian Ries <uilianries@gmail.com>
2016-04-25 21:27:40 -0300

    Update Toolchain file
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- Toolchain-bbb-arm.cmake
+++ Toolchain-bbb-arm.cmake
@@ -2,15 +2,17 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 #this one not so much
 SET(CMAKE_SYSTEM_VERSION 1)
+# ARM arch
+SET(CMAKE_SYSTEM_PROCESSOR arm)
 
 SET(TOOLCHAIN_PATH /opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   ${TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER   arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH "${TOOLCHAIN_PATH} ${TOOLCHAIN_PATH}/include")
+SET(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_PATH})
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
